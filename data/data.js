dbcbc98c2bac9581033ef685e22be9c5feabf673
mmd-yavarii <mdyavarii@gmail.com>
2025-06-08 08:41:29 +0330

    create api and dynamic api

--- data/data.js
+++ data/data.js
@@ -1,11 +1,13 @@
-const todos = [
-  {
-    id: string,
-    title: string,
-    description: string,
-    isCompleted: boolean,
-    createdAt: Date,
-    priority: 'low' | 'medium' | 'high',
-    tags: [],
-  },
-];
+let todos = [];
+
+// {
+//     id: string,
+//     title: string,
+//     description: string,
+//     isCompleted: boolean,
+//     createdAt: Date,
+//     priority: 'low' | 'medium' | 'high',
+//     tags: [],
+//   },
+
+export default todos;
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,23 +1,20 @@
+import todos from '@/data/data';
+import { v4 as uuidv4 } from 'uuid';
+
 export default function handler(req, res) {
   switch (req.method) {
     case 'GET':
-      // code
+      res.status(200).json(todos);
       break;
 
-    case 'POST':
-      // code
+    case 'POST': {
+      const data = req.body;
+      if ('title' in data && 'description' in data && 'priority' in data) {
+        const newTodo = { ...data, id: uuidv4(), isCompleted: false, createdAt: new Date().toISOString() };
+        todos.push(newTodo);
+      }
+      res.status(201).json(newTodo);
       break;
-
-    case 'DELETE':
-      // code
-      break;
-
-    case 'PATCH':
-      //code
-      break;
-
-    case 'PUT':
-      // code
-      break;
+    }
   }
 }
